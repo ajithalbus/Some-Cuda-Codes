0b1411bc10dc35deb79ae485d1c20dc6ac99d8c7
mak <ajithalbus@gmail.com>
2017-11-11 08:38:07 +0530

    working

--- main.cu
+++ main.cu
@@ -8,13 +8,19 @@
 #include <boost/filesystem.hpp>
 #include <string>
 #include "mfcc.h"
-
+#include <omp.h>
 using namespace std;
 using namespace boost::filesystem;
 
 //mfcc
 
 //mfcc end
+char * to_string(int x){
+    char *a=(char *)malloc(2);
+    a[0]=x+48;
+    a[1]='\0';
+    return a;
+}
 
 int argmin(double v[],int n){
     int i,min=0;
@@ -35,18 +41,37 @@ int main(){
         x[i]=i;
     }*/
     //int d=dtw_nv(x,x,3,3);
+    int i;
     string train="/home/ganesh/nw/dtw_cuda/datas/";
     
     string test="/home/ganesh/nw/dtw_cuda/datas/";
-    
-    mfcc one(train+"1/ac_1.mfcc");
-
-    mfcc two(train+"2/ac_2.mfcc");
-    mfcc three(train+"3/ac_3.mfcc");
     string testn;
     cin>>testn;
     
     mfcc testmfcc(test+testn);
+       mfcc* templates=(mfcc *)malloc(sizeof(mfcc)*10);
+    
+    #pragma omp parallel for 
+    for(i=1;i<=9;i++){
+        templates[i-1]=mfcc(train+to_string(i)+"/ac_"+to_string(i)+".mfcc");
+        //cout<<"D-READ "<<i<<endl;
+    }
+    //cout<<train+to_string(1)+"/ac_"+to_string(1)+".mfcc";
+    //cout<<dtw_nv(templates[0],testmfcc);
+    
+    double d[9];
+    #pragma omp parallel for
+    for(i=0;i<9;i++){
+        d[i]=dtw_nv(testmfcc,templates[i]);
+        //cout<<"D-TEST "<<i<<endl;
+    }
+    cout<<argmin(d,9);
+    /*
+        mfcc one(train+"1/ac_1.mfcc");
+
+    mfcc two(train+"2/ac_2.mfcc");
+    mfcc three(train+"3/ac_3.mfcc");
+    string testn;
     
     double d[3];
     
@@ -56,7 +81,7 @@ int main(){
 
     cout<<argmin(d,3);
 
-    
+    */
 
     //cout<<d;  
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 main: mfcc.o kernel.o prefix.o dtw.o main.o
-	nvcc -arch=sm_20 -lboost_system mfcc.o kernel.o prefix.o dtw.o main.o -o run
+	nvcc -arch=sm_20 -lboost_system -Xcompiler -fopenmp mfcc.o kernel.o prefix.o dtw.o main.o -o run
 
 dtw.o: dtw.cu dtw.h
 	nvcc -arch=sm_20 -c dtw.cu
@@ -14,7 +14,7 @@ mfcc.o: mfcc.cu
 	nvcc -arch=sm_20 -c mfcc.cu
 
 main.o: main.cu
-	nvcc -arch=sm_20 -c main.cu
+	nvcc -arch=sm_20 -Xcompiler -fopenmp -c main.cu
 
 clean: 
 	rm -rf *.o run
