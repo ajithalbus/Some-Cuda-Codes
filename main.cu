f50d76b02d002111211216dc47427c8f00a37144
mak <ajithalbus@gmail.com>
2017-11-12 05:41:43 +0530

    winding up

--- main.cu
+++ main.cu
@@ -35,36 +35,59 @@ int argmin(double v[],int n){
     return min;
 }
 
+int argmin_val(double v[],int n){
+    int i,min=0;
+    double minval=1000000.0;
+    for(i=0;i<n;i++){
+        if(v[i]<minval){
+            minval=v[i];
+            //cout<<v[i]<<' ';
+            min=i+1;
+        }
+    }
+    return minval;
+}
+
 int main(){
     /*int x[]={1,2,3,4,5},y[]={2,4,6,2};
     int d=dtw_nv(x,y,5,4);
     //cout<<d;
     
     */
-    int i;
+    int i,j;
     string train="/home/ganesh/nw/dtw_cuda/datas/";
     
-    string test="/home/ganesh/nw/dtw_cuda/datas/";
+    string test="/home/ganesh/nw/dtw_cuda/datas_test/";
     string testn;
+    cout<<"Enter test file :: ";
     cin>>testn;
     
     mfcc testmfcc(test+testn);
-       mfcc* templates=(mfcc *)malloc(sizeof(mfcc)*10);
-    
-    #pragma omp parallel for 
+
+    mfcc* templates=(mfcc *)malloc(sizeof(mfcc)*30);
+    //omp_set_num_threads(30);
+    double d[10];
     for(i=1;i<=9;i++){
-        templates[i-1]=mfcc(train+to_string(i)+"/ac_"+to_string(i)+".mfcc");
-        //cout<<"D-READ "<<i<<endl;
+        double temp_d[30];
+        
+        for(j=0;j<30;j++){
+            templates[j]=mfcc(train+to_string(i)+"/"+to_string(j)+".mfcc");
+        }
+
+        #pragma omp parallel for
+        for(j=0;j<30;j++){
+            temp_d[j]=dtw_nv(testmfcc,templates[j]);
+        }
+        #pragma omp barrier
+        #pragma omp flush
+        
+        d[i-1]=argmin_val(temp_d,30);
     }
-    //cout<<train+to_string(1)+"/ac_"+to_string(1)+".mfcc";
-    //cout<<dtw_nv(templates[0],testmfcc);
+
+    cout<<endl;
+    /*for(i=0;i<9;i++)
+    cout<<"diff-score - "<<i+1<<'-'<<d[i]<<endl;
+*/
+    cout<<"Predicted label :: "<<argmin(d,9)<<endl;
     
-    double d[9];
-    #pragma omp parallel for
-    for(i=0;i<9;i++){
-        d[i]=dtw_nv(testmfcc,templates[i]);
-        //cout<<"D-TEST "<<i<<endl;
-    }
-    cout<<argmin(d,9);
-      
 }
